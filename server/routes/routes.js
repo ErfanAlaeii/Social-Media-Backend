2e8429fd5fba16f0d6364f93063bf4264cc38a10
Erfan <erfanalaei2001@gmail.com>
2025-04-28 00:16:12 +0330

    add authRoutes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import userRoutes from './userRoutes.js'
+import authRoutes from './authRoutes.js'
 
 const router = Router()
 
 const baseURL = "api/v1";
 
 router.use(`/${baseURL}/users`,userRoutes)
+router.use(`/${baseURL}/auth`,authRoutes)
 
 export default router;
